b02a2ecfb7e8fe595333052109f6ad05c768fce2
FrancoisHaegel <haegelfrancois@yahoo.fr>
2020-01-29 09:10:58 +0100

    still trying to fix

--- index.php
+++ index.php
@@ -35,6 +35,19 @@ $jwt = new Slim\Middleware\JwtAuthentication([
 
 $app->add($jwt);
 
+//Enable lazy CORS
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            // ->withHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 function getAllUsers($request, $response, $args) {
     global $entityManager;
     $userRepository = $entityManager->getRepository('Users');
