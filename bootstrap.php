89ff6b89d43e8773931a126d6da7bb7152a9eea7
FrancoisHaegel <haegelfrancois@yahoo.fr>
2020-01-29 08:42:56 +0100

    fix scalingo url

--- bootstrap.php
+++ bootstrap.php
@@ -6,11 +6,6 @@ require_once "vendor/autoload.php";
 $isDevMode = true;
 $config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 $conn = array(
-'driver' => 'pdo_mysql',
-'host'=> 'cd4a2220-da2c-4230-8968-4d8c43c687a5.projet-web-b-1610.mysql.dbs.scalingo.com:',
-'user' => 'projet_web_b_1610',
-'password' => 'zEy7VGdSbhw-nGnHriTn',
-'dbname' => 'projet_web_b_1610',
-'port' => '33571'
+'url' => 'mysql://projet_web_b_1610:zEy7VGdSbhw-nGnHriTn@cd4a2220-da2c-4230-8968-4d8c43c687a5.projet-web-b-1610.mysql.dbs.scalingo.com:33571/projet_web_b_1610?useSSL=true&verifyServerCertificate=false'
 );
 $entityManager = EntityManager::create($conn, $config);
