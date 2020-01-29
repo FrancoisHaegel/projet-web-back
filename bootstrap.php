196ad3c130c7629fadbbecfc3c0a4e68b6152cee
FrancoisHaegel <haegelfrancois@yahoo.fr>
2020-01-29 08:51:48 +0100

    config

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,6 @@ require_once "vendor/autoload.php";
 $isDevMode = true;
 $config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 $conn = array(
-'url' => 'mysql://projet_web_b_1610:zEy7VGdSbhw-nGnHriTn@cd4a2220-da2c-4230-8968-4d8c43c687a5.projet-web-b-1610.mysql.dbs.scalingo.com:33571/projet_web_b_1610?useSSL=true&verifyServerCertificate=false'
+'url' => 'mysql://projet_web_b_1610:zEy7VGdSbhw-nGnHriTn@cd4a2220-da2c-4230-8968-4d8c43c687a5.projet-web-b-1610.mysql.dbs.scalingo.com:33571/projet_web_b_1610?useSSL=true&verifyServerCertificate=false',
 );
 $entityManager = EntityManager::create($conn, $config);
